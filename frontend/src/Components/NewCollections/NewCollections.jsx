08dbbaa4f0f09d9ad1ad2e694cfbe368ca1124f8
fionitachavella <fionitachavella.13@gmail.com>
2024-07-24 06:33:37 +0700

    NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -11,7 +11,7 @@ const NewCollections = () =>{
     const [new_collection,setNew_collection] = useState([]);
 
     useEffect(()=>{
-        fetch('http://localhost:4000/newcollections')
+        fetch('http://54.172.100.247:3000/newcollections')
         .then((response)=>response.json())
         .then((data)=>setNew_collection(data));
     },[])  
